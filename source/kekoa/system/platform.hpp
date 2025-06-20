a8506a04280c0705f00b2e9f6e9d27e9290e7873
Brody Liudzius <brodyliudzius@gmail.com>
2025-06-19 21:13:48 -0700

    Added defines

--- source/kekoa/system/platform.hpp
+++ source/kekoa/system/platform.hpp
@@ -12,19 +12,46 @@
 
 // Check compiler
 
+// Clang
+#if defined (__clang__)
+
+#endif
+
+// GCC
+
+
+// MSVC
+
+
+
 // Check OS
-#if defined(_WIN32) || defined(WIN32) || defined(__WIN32) || defined(__WIN32__)
+
+// Windows
+#if defined(_WIN32) || defined(WIN32) || defined(__WIN32) || defined(__WIN32__) // Check for Windows
 	#define KEKOA_WINDOWS
-	#if defined(_WIN64) || defined(WIN64) | defined(__WIN64) || defined(__WIN64__)
+	#if defined(_WIN64) || defined(WIN64) | defined(__WIN64) || defined(__WIN64__) // Check for 64-bit Windows
 		#define KEKOA_WINDOWS_64_BIT
-	#else
+	#else // Windows is 32-bit
 		#define KEKOA_WINDOWS_32_BIT
+	#endif
+#endif // Check for Windows
+
+// Unix, Linux
+#if defined(unix) || defined (__unix) || defined(__unix__)
+	#if defined(linux) || defined(__linux) || defined(__linux__)
+
 	#endif
 #endif
 
+
+
 // Check Architecture
 
+
+
+
 // Check C++ version
+
 
 
 #endif //KEOKA_PLATFORM_HPP_INCLUDED
